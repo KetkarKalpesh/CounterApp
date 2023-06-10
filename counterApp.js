b6c325a5439bff5ab8b4d2e78f529664b7fe62e7
Kalpesh Ketkar <106992078+KetkarKalpesh@users.noreply.github.com>
2023-06-10 17:20:25 +0530

    First Commit

---
+++ counterApp.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class CounterApp extends LightningElement {
+    counter = 0
+    time = Date()
+
+    handleIncrement(){
+        this.counter = this.counter + 1
+    }
+
+    handleDecrement(){
+        this.counter = this.counter - 1
+    }
+
+    handleReset(){
+        this.counter = 0
+    }
+}
